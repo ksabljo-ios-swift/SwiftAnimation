afe02439a49d8b6e7b6629fb224f0bc90824913a
Krešimir Sabljo <ksabljo@icloud.com>
2019-06-19 16:17:14 +0200

    .initial commit

--- SwiftAnimation/ViewController.swift
+++ SwiftAnimation/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let countingLabel: UILabel =  {
+        let label = UILabel()
+        label.text = "123456789"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+            
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(countingLabel)
+        countingLabel.frame = view.frame
+        
+
+        let displayLink = CADisplayLink(target: self, selector: #selector(handleUpdate))
+        displayLink.add(to: .main, forMode: RunLoop.Mode.default)
     }
 
 
+    var startValue: Double = 0
+    let endValue: Double = 100
+    let animationDuration: Double = 3.5
+    
+    let animationStartTime = Date()
+    
+    @objc func handleUpdate() {
+        
+        let now = Date()
+        let elapsedTime = now.timeIntervalSince(animationStartTime)
+        
+        if elapsedTime > animationDuration {
+            self.countingLabel.text = "\(endValue)"
+        }
+            
+        else {
+            let percentage = elapsedTime / animationDuration
+            let value = startValue + percentage * (endValue - startValue)
+            self.countingLabel.text = "\(value)"
+        }
+        
+        
+        // second
+//        self.countingLabel.text = "\(startValue)"
+//        startValue += 1
+//
+//        if startValue > endValue {
+//            startValue = endValue
+//        }
+//
+     
+        
+        // first
+//        let time = Date().timeIntervalSince1970
+//        print(time)
+//        self.countingLabel.text = "\(time)"
+        
+    }
 }
 
